c87226ff1d83eda9dfcd1234f5f86ffb1c62a9da
Schubert David Rodriguez <scrodrig@thoughtworks.com>
2019-02-16 11:44:08 -0500

    [JBVIDFRAG-000][SR] configures files and folders

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        asas
+      </div >
+    );
+  }
+}
+
+export default Home;
