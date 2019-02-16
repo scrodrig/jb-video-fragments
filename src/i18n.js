47debe3e72fea89b2520ac2ae5b6558b57d53a5f
Schubert David Rodriguez <scrodrig@thoughtworks.com>
2019-02-16 14:53:14 -0500

    [JBVIDFRAG-000][SR] adds language i18n and config

---
+++ src/i18n.js
@@ -0,0 +1,10 @@
+import i18n from 'i18n-js';
+import { getUserLocale } from 'get-user-locale';
+import en from './assets/translations/en.json';
+
+
+i18n.locale = getUserLocale();
+i18n.fallbacks = true;
+i18n.translations = { en };
+
+export default i18n;
