1d088bca27f1e16478e3344018b37608d326fc9b
Schubert David Rodriguez <scrodrig@thoughtworks.com>
2019-02-16 20:53:50 -0500

    [JBVIDFRAG-001][SR] adds menu for drawer

--- src/components/drawer/DrawerMenu.js
+++ src/components/drawer/DrawerMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Icon } from '@rmwc/icon';
 import {
   Drawer,
   DrawerHeader,
@@ -6,32 +7,47 @@ import {
   DrawerTitle,
   DrawerSubtitle,
 } from '@rmwc/drawer';
-
 import {
   List,
   ListItem,
 } from '@rmwc/list';
 
 import '@material/drawer/dist/mdc.drawer.css';
+import '@rmwc/icon/icon.css';
+import './drawerMenu.css';
+import colors from '../../style/colors';
+import I18n from '../../i18n';
 
 class DrawerMenu extends Component {
   render() {
+    const iconStyle = { color: colors.secondary300, paddingRight: 10 };
     return (
-      <div style={{ flex: 1 }}>
+      <div>
         <Drawer>
           <DrawerHeader>
-            <DrawerTitle>DrawerHeader</DrawerTitle>
-            <DrawerSubtitle>Subtitle</DrawerSubtitle>
+            <DrawerTitle>{I18n.t('drawer.header')}</DrawerTitle>
+            <DrawerSubtitle>{I18n.t('drawer.subtitle')}</DrawerSubtitle>
           </DrawerHeader>
           <DrawerContent>
             <List>
-              <ListItem>Cookies</ListItem>
-              <ListItem>Pizza</ListItem>
-              <ListItem>Icecream</ListItem>
-              <ListItem>Icecream</ListItem>
-              <ListItem>Icecream</ListItem>
-              <ListItem>Icecream</ListItem>
-              <ListItem>Icecream</ListItem>
+              <ListItem>
+                <div className="item">
+                  <Icon icon="favorite" style={iconStyle} />
+                  <h4>{I18n.t('drawer.favorites')}</h4>
+                </div>
+              </ListItem>
+              <ListItem>
+                <div className="item">
+                  <Icon icon="search" style={iconStyle} />
+                  <h4>{I18n.t('drawer.search')}</h4>
+                </div>
+              </ListItem>
+              <ListItem>
+                <div className="item">
+                  <Icon icon="lock" style={iconStyle} />
+                  <h4>{I18n.t('drawer.account')}</h4>
+                </div>
+              </ListItem>
             </List>
           </DrawerContent>
         </Drawer>
