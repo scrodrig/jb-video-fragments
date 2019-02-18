6240a75358a699ee6bf2add865228c0b4c806b56
Schubert David Rodriguez <scrodrig@thoughtworks.com>
2019-02-18 15:22:55 -0500

    [JBVIDFRAG-007][SR] Adds markers for video

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Typography } from '@rmwc/typography';
@@ -7,6 +8,43 @@ import './videoPlayer.css';
 import I18n from '../../i18n';
 
 class Video extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [],
+    };
+  }
+
+  componentDidMount() {
+    this.drawMarkers();
+  }
+
+  drawMarkers() {
+    const { context } = this.props;
+    const { playingClip } = context;
+    if (playingClip.type === 'full') {
+      const track = document.getElementById('trackC');
+      const video = document.getElementById('videoC');
+      const controlBar = document.getElementById('bar');
+      track.addEventListener('load', () => {
+        const c = video.textTracks[0].cues;
+        for (let i = 0; i < c.length; i++) {
+          const s = document.createElement('span');
+          s.innerHTML = c[i].text;
+          s.setAttribute('data-start', c[i].startTime);
+          s.addEventListener('click', this.seek);
+          controlBar.appendChild(s);
+        }
+      });
+    }
+  }
+
+  seek() {
+    const video = document.getElementById('videoC');
+    video.currentTime = this.getAttribute('data-start');
+    if (video.paused) { video.play(); }
+  }
+
   render() {
     const { context } = this.props;
     const { playingClip } = context;
@@ -21,15 +59,33 @@ class Video extends Component {
           {I18n.t('clip.title', { clipTitle: playingClip.name })}
         </Typography>
         <video
+          id="videoC"
           src={`/videos/video.mp4#t=${playingClip.start},${playingClip.end}`}
           controls
           // autoPlay
         >
           <track
+            id="trackC"
+            src="videos/markers/chapters.vtt"
+            kind="chapters"
             default
-            kind="captions"
+          />
+          <track
+            src="videos/markers/chapters.vtt"
+            kind="subtitles"
+            default
           />
         </video>
+        {playingClip.type === 'full' ? <div id="bar" /> : null}
+
+        {/* <div> */}
+        {/* {this.state.markers.map(marker => ( */}
+        {/* <span className="marker"> */}
+        {/* {marker.text} */}
+        {/* </span> */}
+        {/* ))} */}
+        {/* </div> */}
+
       </div>
     );
   }
